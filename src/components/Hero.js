5e15004ae4c37b1447ec6a7b6e31e9b6fa682631
Fatimah Hussain <mindblowingmaths@gmail.com>
2022-07-10 15:13:09 -0700

    second commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ export default function Hero() {
     return (
         <main>
         {/* <img className= "hero--image"src="../images/photo-grid.png"/> */}
-        <h1 className="hero--header">Online Experiences</h1>
+        <h1 className="hero--header"> Incredible Experiences</h1>
         <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
 </p>
         </main>
